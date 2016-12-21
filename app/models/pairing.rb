4e9661a0b2bc59affbd5c514d9ecf63e17cd32ec
Dan Roberts <dan@adadevelopersacademy.org>
2016-12-21 11:50:23 -0800

    Test and implement some constraints on pairings

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,4 +2,6 @@ class Pairing < ApplicationRecord
   belongs_to :placement
   belongs_to :company
   belongs_to :student
+
+  validates :student, uniqueness: {scope: :placement}
 end
--- test/models/pairing_test.rb
+++ test/models/pairing_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class PairingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "Constructor success" do
+    pairing_data = {
+      placement: placements(:empty),
+      student: students(:no_company),
+      company: companies(:no_students)
+    }
+    Pairing.create!(pairing_data)
+  end
+
+  test "Cannot build pairing without placement" do
+    pairing_data = {
+      student: students(:no_company),
+      company: companies(:no_students)
+    }
+    p = Pairing.create(pairing_data)
+    assert_not p.valid?
+    assert_includes p.errors.messages, :placement
+  end
+
+  test "Cannot build pairing without student" do
+    pairing_data = {
+      placement: placements(:empty),
+      company: companies(:no_students)
+    }
+    p = Pairing.create(pairing_data)
+    assert_not p.valid?
+    assert_includes p.errors.messages, :student
+  end
+
+  test "Cannot build pairing without company" do
+    pairing_data = {
+      placement: placements(:empty),
+      student: students(:no_company)
+    }
+    p = Pairing.create(pairing_data)
+    assert_not p.valid?
+    assert_includes p.errors.messages, :company
+  end
+
+  test "Cannot double-place a student in a pairing" do
+    pairing_data = {
+      placement: placements(:full),
+      student: students(:anita),
+      company: companies(:no_students)
+    }
+    p = Pairing.create(pairing_data)
+    assert_not p.valid?
+    assert_includes p.errors.messages, :student
+  end
+
+  # Turns out some companies have multiple slots
+  # test "Cannot double-place a company in a pairing" do
+  #   pairing_data = {
+  #     placement: placements(:full_placement),
+  #     student: students(:no_company),
+  #     company: companies(:freedom_inc)
+  #   }
+  #   p = Pairing.create(pairing_data)
+  #   assert_not p.valid?
+  #   assert_includes p.errors.messages, :student
   # end
 end
